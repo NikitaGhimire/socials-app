79af3e71d5eee27ae3128e9d9f7fe15074302c72
NikitaGhimire <nikitaghimire1@gmail.com>
2024-12-22 22:35:06 -0800

    Frontend: AuthContext to handle authentication.

--- frontend/src/context/AuthContex.jsx
+++ frontend/src/context/AuthContex.jsx
@@ -1,10 +1,17 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
 
+    // Load user data from localStorage on initial load (if exists)
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        if (storedUser) {
+            setUser(storedUser);
+        }
+    }, []);
     const login = (userData) => {
         localStorage.setItem("user", JSON.stringify(userData));
         setUser(userData);
@@ -12,11 +19,17 @@ export const AuthProvider = ({children}) => {
 
     const logout = () => {
         localStorage.removeItem("user");
+        localStorage.removeItem("token");
         setUser(null);
     };
 
+    const updateUser = (updatedUserData) => {
+        localStorage.setItem("user", JSON.stringify(updatedUserData));
+        setUser(updatedUserData);
+    };
+
     return (
-        <AuthContext.Provider value={{user, login, logout}}>
+        <AuthContext.Provider value={{user, login, logout, updateUser}}>
             { children }
         </AuthContext.Provider>
     );
