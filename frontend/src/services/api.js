396cc58ac7cc5a01064360137d14d1623e43247f
NikitaGhimire <nikitaghimire1@gmail.com>
2025-03-13 00:26:16 -0700

    use environment variables to dynamically set the baseURL based on whether you're in a development or production environment.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://messaging-app-zyja.onrender.com/api" || "http://localhost:5000/api",
+    baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api",
     headers: {
     'Content-Type': 'application/json',
 }, // Your backend API
